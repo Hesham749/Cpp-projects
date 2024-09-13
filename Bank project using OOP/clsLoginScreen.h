576c84d714a0568158a7a460e63797fd78451b87
Hesham Elsayed <heesham749@gmail.com>
2024-09-13 15:24:01 +0300

    u

--- Bank project using OOP/clsLoginScreen.h
+++ Bank project using OOP/clsLoginScreen.h
@@ -11,20 +11,20 @@ class clsLoginScreen :protected::clsScreen
 	static bool _Login()
 	{
 		bool LoginFailed = false;
-		short NumberOfTries = 0;
+		short NumberOfTrials = 0;
 		do
 		{
 			system("cls");
 			_ScreenHeader("Login Screen", 2);
 			if (LoginFailed)
 			{
-				NumberOfTries++;
+				NumberOfTrials++;
 				cout << "Invalid User Name/Password!\n";
-				printf("You have %d Tries to login\n", 3 - NumberOfTries);
+				printf("You have %d Trial(s) to login\n", 3 - NumberOfTrials);
 			}
-			if (NumberOfTries == 3)
+			if (NumberOfTrials == 3)
 			{
-				cout << "\nYou are locked after 3 failed tries\n\n";
+				cout << "\nYou are locked after 3 Failed Trials\n\n";
 				return false;
 			}
 			printf("Enter User Name ? ");
